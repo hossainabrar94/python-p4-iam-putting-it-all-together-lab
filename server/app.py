4c7a340dd3adbd7a14c82f3bd8067908e532cb44
Abrar <hossainabrar@aol.com>
2024-10-08 19:15:14 -0700

    completed

--- server/app.py
+++ server/app.py
@@ -8,19 +8,88 @@ from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        if not username and not password:
+            return {'Error':'username and password must be submitted'}, 400
+        
+        user = User()
+        for field in ['username', 'image_url', 'bio', 'password']:
+            setattr(user, field, data.get(field))
 
+        try:
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return user.to_dict(), 201
+        except IntegrityError:
+            db.session.rollback()
+            return {'Error': 'username already exists'}, 422
+
+
+# Handles auto-login for time users refresh the page or navigate back to the site from elsewhere.
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+            return user.to_dict(), 200
+        return {'Error': 'Unauthorized'}, 401
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        user = User.query.filter(User.username == request.get_json().get('username')).first()
+        if user and user.authenticate(request.get_json().get('password')):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        else:
+            return {'Error':'Incorrect Username or password'}, 401
+
 
 class Logout(Resource):
-    pass
+    
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {}, 204
+        else:
+            return {'Error': 'User not logged in'}, 401
 
 class RecipeIndex(Resource):
-    pass
+    
+    def get(self):
+        if session.get('user_id'):
+            recipes = [recipe.to_dict() for recipe in Recipe.query.all()]
+            return recipes, 200
+        else:
+            return {'Error': 'Please Log In to view recipes'}, 401
+    
+    def post(self):
+        user_id = session.get('user_id')
+        
+        if not user_id:
+            return {'Error':'Must be signed in'}, 401
+        
+        data = request.get_json()
+        try:
+            recipe = Recipe()
+            for field in ['title', 'instructions', 'minutes_to_complete']:
+                setattr(recipe, field, data.get(field))
+            recipe.user_id = user_id  # Move outside the loop
+            
+            db.session.add(recipe)
+            db.session.commit()
+            return recipe.to_dict(), 201
+        except ValueError as ve:
+            db.session.rollback()
+            return {'error': str(ve)}, 422
+
+
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -7,9 +7,46 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    serialize_rules = ('-recipes.user', )
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, nullable = False, unique = True)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    # Creating one-to-many relationship - a user can have many recipes
+    recipes = db.relationship('Recipe', back_populates = 'user', cascade = 'all')
+
+    @property
+    def password(self):
+        raise AttributeError('Not a readable attribute')
+    @password.setter
+    def password(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password)
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
     
-    pass
+    serialize_rules = ('-user.recipes', )
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable = False)
+    instructions = db.Column(db.String, db.CheckConstraint('LENGTH(instructions) > 49'), nullable = False)
+    minutes_to_complete = db.Column(db.Integer)
+    
+    # Foreign Key created on the many side to point to 1 user
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    # Relying solely on database constraints might not provide user-friendly error messages. Use the @validates decorator to enforce the minimum length at the instance level
+    @validates('instructions')
+    def validate_instructions(self, key, instructions):
+        if len(instructions)<=49:
+            raise ValueError('Instructions must be at least 50 characters')
+        return instructions
+
+    # closing the loop of the one-to-many relationship
+    user = db.relationship('User', back_populates='recipes')
